d84ba3a94f93ba7443d1e63a92cee515e3f7e963
Oleh <olegsimfi@gmail.com>
2025-09-01 18:00:06 +0300

    Update wdio.conf.js and package.json

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,11 +1,8 @@
 import { browser, driver, expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
 
 describe('First test', () => {
     it('test example', async () => {
-        await browser.pause(3000);
-        
+        await browser.pause(3000);    
     })
 })
 
